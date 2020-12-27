f6cc051465da49506c3721b22776bf33b8bc2ad3
Rutu <rutudhokchaule@gmail.com>
2020-12-27 12:45:30 +0530

    step 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
-
+import axios from 'axios';
 import Layout from 'components/Layout';
 import Container from 'components/Container';
 import Map from 'components/Map';
-import Snippet from 'components/Snippet';
 
 
 const LOCATION = {
@@ -18,12 +17,42 @@ const DEFAULT_ZOOM = 2;
 /**
  * MapEffect
  * @description This is an example of creating an effect used to zoom in and set a popup on load
- 
-
 */
-const MapEffect = ({leafletElement: map}) => {
-};
-//async function MapEffect({ leafletElement: map } = {}) {}
+//const MapEffect = ({leafletElement: map}) => {
+//};
+async function MapEffect({ leafletElement: map } = {}) {
+  let response;
+  try{
+    response = await axios.get('https://corona.lmao.ninja/v3/covid-19/countries');
+  }
+  catch(e){
+    console.log(`Failed to fetch countries: ${e.message}`,e);
+    return;
+  }
+  const { data = [] } = response;
+
+  
+  const hasData = Array.isArray(data) && data.length > 0;
+  if(!hasData) return;
+
+  const geoJson = {
+    type: 'FeatureCollection',
+    features: data.map((country = {}) => {
+      const{countryInfo = {} } = country;
+      const{lat,long: lng} = countryInfo;
+      return{
+        type: 'Feature',
+        properties: {
+          ...country,
+        },
+        geometry: {
+          type: 'Point',
+          coordinates:[lng,lat]
+        }
+      }
+    })
+  } 
+}
 
 const IndexPage = () => {
 
@@ -31,6 +60,7 @@ const IndexPage = () => {
     center: CENTER,
     defaultBaseMap: 'OpenStreetMap',
     zoom: DEFAULT_ZOOM,
+    MapEffect
   };
 
   return (
@@ -43,10 +73,6 @@ const IndexPage = () => {
       </Map>
 
       <Container type="content" className="text-center home-start">
-        <h2>Still Getting Started?</h2>
-        <p>Run the following in your terminal!</p>
-        <Snippet>gatsby new [directory] https://github.com/colbyfayock/gatsby-starter-leaflet</Snippet>
-        <p className="note">Note: Gatsby CLI required globally for the above command</p>
       </Container>
     </Layout>
   );
